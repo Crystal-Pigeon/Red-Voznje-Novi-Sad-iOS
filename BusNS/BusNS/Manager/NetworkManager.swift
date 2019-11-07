6e78e85a2ecb00ca97b113b9ad5bf6f9736d3e5f
Marko Popić <popic.mm@gmail.com>
2019-11-07 14:16:57 +0100

    #BUNS-9 Optimization for caching

---
+++ BusNS/BusNS/Manager/NetworkManager.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkManager.swift
+//  BusNS
+//
+//  Created by Marko Popić on 11/7/19.
+//  Copyright © 2019 Crystal Pigeon. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+public class NetworkManager {
+    
+    private init(){}
+    
+    public static let shared = NetworkManager()
+    
+    public func isInternetAvailable() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+}

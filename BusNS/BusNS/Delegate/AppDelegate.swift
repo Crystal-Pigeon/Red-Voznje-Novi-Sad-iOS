e380a46a157a73e441e3de751b468fa065318fc5
Marko Popić <popic.mm@gmail.com>
2020-03-11 01:25:59 +0100

    Removed SceneDelegate

--- BusNS/BusNS/Delegate/AppDelegate.swift
+++ BusNS/BusNS/Delegate/AppDelegate.swift
@@ -60,22 +60,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
     
     func applicationWillTerminate(_ application: UIApplication) {
         if !StorageManager.fileExists(StorageKeys.urbanLines, in: .caches) || !StorageManager.fileExists(StorageKeys.suburbanLines, in: .caches) {
@@ -96,6 +80,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 return
             }
         }
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        //get main vc as currently active vc and refresh current hour and day
     }
 }
 
